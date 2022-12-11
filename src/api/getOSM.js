3a97ca2f54f75b1a9888bdfe509373be2780a7cf
Arjun R <aradhakrishnan@wagepoint.com>
2022-12-11 21:48:20 +0530

    clean up and fix

--- src/api/getOSM.js
+++ src/api/getOSM.js
@@ -3,19 +3,19 @@ import axios from 'axios';
 const URL = 'https://www.openstreetmap.org/api/0.6/map';
 
 const getOSM = async (data) => {
-	const params = { bbox: `${data.left},${data.bottom},${data.right},${data.top}` };
-	try {
-		const res = await axios.get(URL, { params });
-		return {
-			error: null,
-			data: res.data,
-		};
-	} catch (err) {
-		return {
-			error: err,
-			data: null,
-		};
-	}
+  const params = { bbox: `${data.left},${data.bottom},${data.right},${data.top}` };
+  try {
+    const res = await axios.get(URL, { params });
+    return {
+      error: null,
+      data: res.data
+    };
+  } catch (err) {
+    return {
+      error: err,
+      data: null
+    };
+  }
 };
 
 export { getOSM };
